fc5a45e3e6cc94af18ddf97264afe1c9f12077bb
Thiago Resende <thiago.gcresende@gmail.com>
2018-07-07 13:54:58 -0300

    Integridade do projeto

--- cap1/projeto/gulpfile.js
+++ cap1/projeto/gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp'),
+    clean = require('gulp-clean'),
     imagemin = require('gulp-imagemin');
+
+gulp.task('copy', function () {
+    gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'))
+});
 
 gulp.task('build-img', function () {
     gulp.src('src/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('src/img'))
+        .pipe(gulp.dest('dist/img'))
 });
