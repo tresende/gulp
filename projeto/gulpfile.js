74a4fb86ed39ffd1ba0dadd0705edf9217736ff0
Thiago Resende <thiago.gcresende@gmail.com>
2018-07-08 10:09:37 -0300

    usemin

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp'),
     clean = require('gulp-clean'),
-    concat = require('gulp-concat'),
-    htmlReplace = require('gulp-html-replace'),
-    htmlReplace = require('gulp-html-replace'),
     uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    cssmin = require('gulp-cssmin'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('copy', ['clean'], function () {
@@ -24,25 +23,15 @@ gulp.task('build-img', function () {
         .pipe(gulp.dest('dist/img'))
 });
 
-gulp.task('build-js', function () {
-    var stream = gulp.src(['dist/js/jquery.js',
-        'dist/js/home.js',
-        'dist/js/ativa-filtro.js'])
-        .pipe(concat('all.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-    return stream
-});
-
-gulp.task('build-html', ['build-js'], function () {
+gulp.task('usemin', function () {
     var stream = gulp.src('dist/**/*.html')
-        .pipe(htmlReplace({
-            js: 'js/all.js'
+        .pipe(usemin({
+            'js': [uglify],
+            'css': [cssmin]
         }))
         .pipe(gulp.dest('dist'))
-    return stream
 });
 
 gulp.task('default', ['copy'], function () {
-    gulp.start('build-img', 'build-js', 'build-html',);
+    gulp.start('build-img', 'usemin', );
 });
