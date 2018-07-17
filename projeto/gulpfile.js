a8ac92d40487772fdb08ae0ef46ca10815534b98
Thiago Resende <thiago.gcresende@gmail.com>
2018-07-17 06:58:49 -0300

    lint

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,10 +1,13 @@
-var gulp = require('gulp'),
-    clean = require('gulp-clean'),
-    uglify = require('gulp-uglify'),
-    usemin = require('gulp-usemin'),
-    cssmin = require('gulp-cssmin'),
-    browserSync = require('browser-sync'),
-    imagemin = require('gulp-imagemin');
+var gulp = require('gulp')
+    , clean = require('gulp-clean')
+    , uglify = require('gulp-uglify')
+    , usemin = require('gulp-usemin')
+    , cssmin = require('gulp-cssmin')
+    , browserSync = require('browser-sync')
+    , jshint = require('gulp-jshint')
+    , imagemin = require('gulp-imagemin')
+    , csslint = require('gulp-csslint')
+    , jshintStylish = require('jshint-stylish');
 
 gulp.task('copy', ['clean'], function () {
     var stream = gulp.src('src/**/*')
@@ -37,13 +40,26 @@ gulp.task('default', ['copy'], function () {
     gulp.start('build-img', 'usemin', );
 });
 
-
-
 gulp.task('server', function () {
     browserSync.init({
         server: {
             baseDir: 'src'
         }
+    });
+
+    gulp.watch('src/js/*.js').on('change', function (event) {
+        console.log(event.path)
+        gulp.src(event.path)
+            .pipe(jshint())
+            .pipe(jshint.reporter(jshintStylish));
+    });
+
+    // novidade para lint de css
+    gulp.watch('src/css/**/*.css').on('change', function (event) {
+        console.log("Linting " + event.path);
+        gulp.src(event.path)
+            .pipe(csslint())
+            .pipe(csslint.reporter());
     });
 
     gulp.watch('src/**/*').on('change', browserSync.reload);
